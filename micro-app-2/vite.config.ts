d4b2c3ec8a29406365b36de632026b43aa0192b1
Travis Zhang <wr.zhang25@gmail.com>
2026-04-02 14:01:32 +0800

    feat: update

--- micro-app-2/vite.config.ts
+++ micro-app-2/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import qiankun from 'vite-plugin-qiankun'
 import istanbulPlugin from 'vite-plugin-istanbul'
 import canyonVitePlugin from '@canyonjs/vite-plugin'
+import path from 'node:path'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
